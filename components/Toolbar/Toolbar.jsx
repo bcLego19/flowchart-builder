f41c9fb48fc52c09dc392abc97461bc4af938654
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2025-08-04 17:13:23 -0700

    Update Toolbar to correct syntax error

--- components/Toolbar/Toolbar.jsx
+++ components/Toolbar/Toolbar.jsx
@@ -5,7 +5,7 @@ const Toolbar = ({createNode}) => {
   return (
     <div>
       <h2>Toolbar</h2>
-      <button onClick={createNode()}>Add</button>
+      <button onClick={createNode}>Add</button>
     </div>
   );
 };
